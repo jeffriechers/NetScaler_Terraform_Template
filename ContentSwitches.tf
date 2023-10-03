8ce885774524d70b0640779b9d8a10c26019780b
jeffriechers <71444494+jeffriechers@users.noreply.github.com>
2023-10-03 11:36:48 -0400

    Update ContentSwitches.tf
    Switching key value for LB bindings to prevent name collisions to multiple binding to the same content switch

--- ContentSwitches.tf
+++ ContentSwitches.tf
@@ -17,8 +17,8 @@ resource "citrixadc_cspolicy" "CSW_cspolicy" {
 #Load Balancer bindings
 resource "citrixadc_csvserver_cspolicy_binding" "CSW_policybind" {
   for_each = { for u in var.CSW_policybind : u.name => u}
-    name = each.key
-    policyname = each.value.policyname
+    name = each.value.name
+    policyname = each.key
     priority = each.value.priority
     targetlbvserver = each.value.targetlbvserver
     depends_on = [ citrixadc_lbvserver.lb_vservers, citrixadc_csvserver.CSW_vservers ]
