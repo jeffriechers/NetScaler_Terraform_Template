22a093ebe4195ca299ad2848316d7b3f18500f5c
jeffriechers <jeffriechers@gmail.com>
2023-10-10 12:38:37 -0400

    Added Responder Actions for Dropping Non-US or Non-US and Canada locations.

--- Responder.tf
+++ Responder.tf
@@ -14,6 +14,20 @@ resource "citrixadc_responderpolicy" "Responder_Policy" {
   rule = each.value.rule
   depends_on = [citrixadc_responderaction.Responder_Action]
 }
+resource "citrixadc_responderpolicy" "Drop_Non_US" {
+  name    = "Drop_Non_US"
+  action = "DROP"
+  rule = "CLIENT.IP.SRC.MATCHES_LOCATION(\"*.US.*.*.*.*\").NOT"
+  logaction = "Non_US_Drop"
+  depends_on = [citrixadc_auditmessageaction.NonUS_MessageAction]
+}
+resource "citrixadc_responderpolicy" "Drop_Non_US_and_Canada" {
+  name    = "Drop_Non_US_and_Canada"
+  action = "DROP"
+  rule = "CLIENT.IP.SRC.MATCHES_LOCATION(\"*.US.*.*.*.*\").NOT && CLIENT.IP.SRC.MATCHES_LOCATION(\"*.CA.*.*.*.*\").NOT"
+  logaction = "Non_USorCanada_Drop"
+  depends_on = [citrixadc_auditmessageaction.NonUSorCanada_MessageAction]
+}
  resource "citrixadc_csvserver_responderpolicy_binding" "CSW_responderbinding" {
   for_each = { for u in var.CSW_responderbinding : u.name => u}
       name = each.key
